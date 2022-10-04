98bf9dedd5a0ee4907cebf0b34b4db0bbb324ffc
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-10-04 14:02:36 -0300

    Update webappEQUIPE01.py

--- webappEQUIPE01.py
+++ webappEQUIPE01.py
@@ -1,82 +1,20 @@
-#libraries
 import streamlit as st
-from PIL import Image
-from io import BytesIO
-import requests
-import pandas as pd
-import altair as alt
-from urllib.error import URLError
-import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import numpy as np
 
-#DÚVIDAS
-rD = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRtt6VlUfp77JA2ok1dUAN5WMj3NNKCliMyG6Tb7Yu8MzUzQ5lZXjcNOWMgit6VaJw8W8lPIzjjnWVn/pub?gid=51090662&single=true&output=csv')
-dataD = rD.content
-dfD = pd.read_csv(BytesIO(dataD), index_col=0)
-NregD = len(dfD)
-dfD.columns = ['email', 'equipe', 'nome', 'duvida', 'obs']
-selecao01D = dfD['equipe']=='Equipe 01'
-df01D = dfD[selecao01D]
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-#RESPOSTAS
-rR = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQw6XD9vI_C4zvZ6W51vut_Ze_D_OSESuXiHh1IAXeBFXRRvyQ7kyFTLbGip1obadjbZHUmaAxvXmnz/pub?gid=1789345467&single=true&output=csv')
-dataR = rR.content
-dfR = pd.read_csv(BytesIO(dataR), index_col=0)
-NregR = len(dfR)
-dfR.columns = ['email', 'equipe', 'nome', 'resposta', 'sugestao']
-selecao01R = dfR['equipe']=='Equipe 01'
-df01R = dfR[selecao01R]
+# Group data together
+hist_data = [x1, x2, x3]
 
-#Cálculo do Número de Registros por EQUIPE
-NregDf01D = len(df01D)
-NregDf01R = len(df01R)
+group_labels = ['Group 1', 'Group 2', 'Group 3']
 
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
 
-image01 = Image.open('ImagemLateral.jpg')
-st.sidebar.image(image01, width=300, caption='Mack Week CCT 2022') 
-st.title("PAINEL - EQUIPE 01")
-menu = ["Dúvidas",
-        "Respostas",
-        "Dúvidas e Respostas"]
-choice = st.sidebar.selectbox("Menu de Opções",menu)
-st.sidebar.info("By: Prof. Massaki de O. Igarashi")
-
-fig = go.Figure(go.Indicator(
-    domain = {'x': [0, 1], 'y': [0, 1]},
-    value = 450,
-    mode = "gauge+number+delta",
-    title = {'text': "Speed"},
-    delta = {'reference': 380},
-    gauge = {'axis': {'range': [None, 500]},
-             'steps' : [
-                 {'range': [0, 250], 'color': "lightgray"},
-                 {'range': [250, 400], 'color': "gray"}],
-             'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 490}}))
-st.fig.show()
-
-if choice == "Dúvidas": 
-    st.header("Relatório de DÚVIDAS")   
-    st.write('EQUIPE 01:')
-    st.warning('Dúvida(s) Enviada(s)')
-    st.code(df01D['duvida']) 
-           
-elif choice == "Respostas":       
-    st.header("Relatório de RESPOSTAS")    
-    st.write('EQUIPE 01:')    
-    st.info('Resposta do(a) TUTOR(A):')
-    st.code(df01R['resposta'])  
-
-               
-elif choice == "Dúvidas e Respostas":       
-    st.header("Relatório: DÚVIDAS E RESPOSTAS")  
-    colDR1, colDR2 = st.columns((1,1))
-    with colDR1:
-        st.write("Nº TOTAL de Dúvidas (DESTA EQUIPE):")
-        st.warning(NregDf01D)
-    with colDR2:
-        st.write("Nº TOTAL de dúvidas RESPONDIDAS:")
-        st.info(NregDf01R)
-    st.subheader('EQUIPE 01:')
-    st.warning('Dúvida(s) Enviada(s)')
-    st.code(df01D['duvida']) 
-    st.info('Resposta do(a) TUTOR(A):')
-    st.code(df01R['resposta'])  
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
