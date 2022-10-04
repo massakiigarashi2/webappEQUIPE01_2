b8a81fee37d7b4475eb286f7e891c8b35776a0bf
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-10-04 14:00:54 -0300

    Update webappEQUIPE01.py

--- webappEQUIPE01.py
+++ webappEQUIPE01.py
@@ -40,6 +40,19 @@ menu = ["Dúvidas",
 choice = st.sidebar.selectbox("Menu de Opções",menu)
 st.sidebar.info("By: Prof. Massaki de O. Igarashi")
 
+fig = go.Figure(go.Indicator(
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    value = 450,
+    mode = "gauge+number+delta",
+    title = {'text': "Speed"},
+    delta = {'reference': 380},
+    gauge = {'axis': {'range': [None, 500]},
+             'steps' : [
+                 {'range': [0, 250], 'color': "lightgray"},
+                 {'range': [250, 400], 'color': "gray"}],
+             'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 490}}))
+st.fig.show()
+
 if choice == "Dúvidas": 
     st.header("Relatório de DÚVIDAS")   
     st.write('EQUIPE 01:')
